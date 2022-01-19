bed3aab3a105379377bc24f5f2467fc058ff4cce
Navaneeth <navaneeth@apples-MacBook-Pro.local>
2022-01-19 15:06:00 +0530

    fixed constant typo

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const GRAPHQL_ENDPOINT = "https://demo.nexpwa.com/graphql/"
+const GRAPHQL_ENDPOINT = "http://demo.nexpwa.com/graphql/"
 module.exports = function (app) {
   app.use(
     '/graphql',
     createProxyMiddleware({
-      target: GRAPHLQL_ENDPOINT,
+      target: GRAPHQL_ENDPOINT,
       changeOrigin: true,
     })
   );
